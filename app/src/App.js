f270697184fc19a68f4c55095574adcbf3fa700c
nuvallo <nuvallo.inc@gmail.com>
2020-03-28 21:10:51 -0400

    making request to api

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get(
+        "https://www.freelancer.com/api/projects/0.1/jobs/search/?job_names%5B%5D=PHP&job_names%5B%5D=website%20design"
+      )
+      .then(res => console.log(res.data.result))
+      .catch(err => console.log("API request App: ", err));
+  }, []);
+
   return (
     <div className="App">
-     <h1>Hello</h1>
+      <h1>Hello</h1>
     </div>
   );
 }
